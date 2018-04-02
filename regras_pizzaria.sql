678799f0ee2a385a14bd0a2054449ea4de727419
Cloves Adriano Paiva Sousa <clovesgtx@gmail.com>
2018-04-02 19:54:03 -0300

    término seção 4

---
+++ regras_pizzaria.sql
@@ -0,0 +1,9 @@
+
+SELECT GetDiaSemana(data_pedido) as dia,
+		GetPeriodoVenda(hora_pedido) as periodo,
+		tipo_entrega,
+		GetBorda(valor_borda) as borda,
+		GetRefrigerante(valor_refrigerante) as refrigerante,
+		GetFaixaValor(valor_total) as valor_total,
+		GetFaixaTempo(minute(tempo)) as tempo
+from pedidos;
--- script_regras.sql
+++ script_regras.sql
@@ -90,6 +90,68 @@ SELECT valor_borda, GetBorda(valor_borda),
 		valor_refrigerante, GetRefrigerante(valor_refrigerante)
 from pedidos;
 
+SELECT valor_total from pedidos;
+
+delimiter $$
+create function GetFaixaValor(valor_total float) returns varchar (10)
+begin
+  declare faixa varchar(10);
+  if (valor_total >= 10 and valor_total < 14) then
+    set faixa = '10-14';
+  elseif (valor_total >= 14 and valor_total < 18) then
+    set faixa = '14-18';
+  elseif (valor_total >= 18 and valor_total < 22) then
+    set faixa = '18-22';
+  elseif (valor_total >= 22 and valor_total < 26) then
+    set faixa = '22-26';
+  elseif (valor_total >= 26 and valor_total < 30) then
+    set faixa = '26-30';
+  elseif (valor_total >= 30 and valor_total < 34) then
+    set faixa = '30-34';
+  elseif (valor_total >= 34 and valor_total < 38) then
+    set faixa = '34-38';
+  elseif (valor_total >= 38 and valor_total < 42) then
+    set faixa = '38-42';
+  elseif (valor_total >= 42 and valor_total < 46) then
+    set faixa = '42-46';
+  elseif (valor_total >= 46 and valor_total <= 50) then
+    set faixa = '46-50';
+  end if;
+
+  return faixa;
+end
+delimiter $$;
+
+SELECT count(valor_total), GetFaixaValor(valor_total) from pedidos
+GROUP BY GetFaixaValor(valor_total);
+
+SELECT MAX(tempo), min(tempo) from pedidos
+
+SELECT MINUTE(tempo) from pedidos
+
+delimiter $$
+create function GetFaixaTempo(minutos float) returns varchar (10)
+begin
+  declare faixa varchar(10);
+  if (minutos >= 10 and minutos < 22) then
+    set faixa = '10-22';
+  elseif (minutos >= 22 and minutos < 33) then
+    set faixa = '22-33';
+  elseif (minutos >= 33 and minutos < 44) then
+    set faixa = '33-44';
+  elseif (minutos >= 44 and minutos <= 55) then
+    set faixa = '44-55';
+  end if;
+
+  return faixa;
+end 
+delimiter $$;
+
+SELECT tempo,
+	GetFaixaTempo(MINUTE(tempo))
+from pedidos;
+
+
 
 
 
